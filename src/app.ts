ea7070af79f2e202567d2125346d4320cbe5ac80
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-06-22 00:31:04 +0530

    chore : "Adde http-errors package and global error handler middleware, updated config to include NODE_ENV"

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 //Routes
 
-
+app.use(globalErrorHandler);
 export default app;
