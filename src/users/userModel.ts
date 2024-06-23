e8c6f3deea1059d4aa02d6b71e11d39aa012bf3c
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-06-23 12:17:27 +0530

    add user model

---
+++ src/users/userModel.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+import { User } from "./userTypes";
+
+const userSchema = new Schema<User>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model<User>("User", userSchema);
+export default User;
