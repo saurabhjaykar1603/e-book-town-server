825d6a2739c48a05bb0a5ef40c947e2482f04f15
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-06-23 19:52:18 +0530

    "Add Cloudinary configuration and dependency"

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,6 +7,9 @@ const _config = {
   databaseUrl: process.env.MONGO_URI,
   env: process.env.NODE_ENV,
   jwtSecret: process.env.JWT_SECRET,
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_API_KEY,
+  apiSecret: process.env.CLOUDINARY_API_SECRET,
 };
 
 export const config = Object.freeze(_config);
