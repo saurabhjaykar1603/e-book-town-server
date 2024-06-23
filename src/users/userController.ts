41c3981c55b3d179f9470312c135a39c6deb7d67
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-06-23 15:37:19 +0530

    "Add loginUser function to userController and corresponding route to userRouter"

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -48,4 +48,45 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(createHttpError(500, "Error while signing the jwt token"));
   }
 };
-export { createUser };
+
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  // Validate request body
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
+
+  try {
+    // Find user by email
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return next(createHttpError(404, "User not found"));
+    }
+
+    // Compare passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return next(createHttpError(401, "Invalid credentials"));
+    }
+
+    // Generate JWT token
+    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+      expiresIn: "1h",
+    });
+
+    // Respond with the token
+    return res.status(200).json({
+      accessToken: token,
+      message: "User logged in successfully",
+    });
+  } catch (error) {
+    // Handle any errors
+    if (error instanceof createHttpError.HttpError) {
+      return next(error);
+    }
+    return next(createHttpError(500, "Error while logging in"));
+  }
+};
+
+export { createUser, loginUser };
