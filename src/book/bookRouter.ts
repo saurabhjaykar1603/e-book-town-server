a5fc8a2f952e91172c3299c3604f5065fc1a899f
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-06-23 16:30:19 +0530

    "Add multer dependency and configured file upload in bookRouter"

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,8 +1,30 @@
 import { Router } from "express";
 import { createBook } from "./bookController";
+import multer from "multer";
+import path from "node:path";
 
 const bookRouter = Router();
+
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+  limits: {
+    fileSize: 3e7,
+  },
+});
 // routes
-bookRouter.post("/create", createBook);
+bookRouter.post(
+  "/create",
+  upload.fields([
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+    {
+      name: "file",
+      maxCount: 1,
+    },
+  ]),
+  createBook
+);
 
 export default bookRouter;
