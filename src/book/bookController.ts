58196a98e570a58e719d6ab3c320292bfe4a4207
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-06-26 00:00:53 +0530

    feat(api): create or add book endpoint

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -2,11 +2,29 @@ import { Request, Response, NextFunction } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
+import fs from "fs";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  res.json({});
+  const { title, genre } = req.body;
+
+  // Validate title and genre
+  if (!title || typeof title !== "string") {
+    return next(createHttpError(400, "Invalid or missing title"));
+  }
+
+  if (!genre || typeof genre !== "string") {
+    return next(createHttpError(400, "Invalid or missing genre"));
+  }
+
   const files = req.files as { [fieldname: string]: Express.Multer.File[] };
-  console.log("hi");
+  if (!files || !files.coverImage || !files.coverImage[0]) {
+    return next(createHttpError(400, "Missing cover image"));
+  }
+
+  if (!files || !files.file || !files.file[0]) {
+    return next(createHttpError(400, "Missing book PDF"));
+  }
 
   const coverImageFileName = files.coverImage[0].filename;
   const filePath = path.resolve(
@@ -15,7 +33,7 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     coverImageFileName
   );
   const coverImageMineType = files.coverImage[0].mimetype.split("/").at(-1);
-
+  /////////////////////////
   const bookPdfFileName = files.file[0].filename;
   const bookPdfPath = path.resolve(
     __dirname,
@@ -38,10 +56,25 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
         format: "pdf",
       }
     );
-    console.log("upload book pdf result", uploadBookPdfFileResult);
-    console.log("upload result", uploadResult);
+    const newBook = await bookModel.create({
+      title,
+      author: "6677d2454b22eea9f357b582",
+      genre,
+      coverImage: uploadResult.secure_url,
+      file: uploadBookPdfFileResult.secure_url,
+    });
+    await fs.promises.unlink(filePath);
+    await fs.promises.unlink(bookPdfPath);
+    return res.status(201).json({
+      message: "Book created successfully",
+      data: newBook._id,
+    });
+    // console.log("upload book pdf result", uploadBookPdfFileResult);
+    // console.log("upload result", uploadResult);
   } catch (error) {
     const err = createHttpError(500, "Error while uploading file");
+    console.log(error);
+
     return next(err);
   }
 };
