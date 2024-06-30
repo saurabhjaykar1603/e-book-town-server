959d5ab536541f66f28a9ba5c78a16d8a4456d93
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-06-30 21:42:28 +0530

    "Add CORS support, updated .env.sample, and modified package.json and package-lock.json to include cors dependency"

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,17 @@ import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./users/userRouter";
 import bookRouter from "./book/bookRouter";
+import cors from "cors";
+import { config } from "./config/config";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(
+  cors({
+    origin: config.frontedDomain,
+  })
+);
 //Routes
 app.use("/api/users", userRouter);
 app.use("/api/books", bookRouter);
--- src/config/config.ts
+++ src/config/config.ts
@@ -10,6 +10,7 @@ const _config = {
   cloudName: process.env.CLOUDINARY_CLOUD_NAME,
   apiKey: process.env.CLOUDINARY_API_KEY,
   apiSecret: process.env.CLOUDINARY_API_SECRET,
+  frontedDomain: process.env.FRONTEND_DOMAIN,
 };
 
 export const config = Object.freeze(_config);
